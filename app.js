5a323d1605170353172e3c238d2203a87234f5b8
Diego-1984 <diegomenoruceta@gmail.com>
2023-02-14 10:34:35 +0000

    Falta ajustar lla imagen del fondo

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const who = ["The dog", "My granma", "His turtle", "My bird"];
 const what = ["eat", "pissed", "crushed", "broked"];
 const when = [
-  "before the class",
-  "right in time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
+  "before the class!",
+  "right in time!",
+  "when I finished!",
+  "during my lunch!",
+  "while I was praying!"
 ];
 
 function getRandomNumber(arr) {
@@ -14,10 +14,26 @@ function getRandomNumber(arr) {
 }
 
 function getExcuse() {
-  let whoindex = getRandomNumber(who);
-  let whatindex = getRandomNumber(what);
-  let whenindex = getRandomNumber(when);
-  let Excuse = whoindex + " " + whatindex + " " + whenindex;
+  let whoindex = getWho();
+  let whatindex = getWhat();
+  let whenindex = getWhen();
+  let Excuse = getCompleteExcuse(whoindex, whatindex, whenindex);
 
   document.getElementById("ExcusaCompleta").innerHTML = Excuse;
 }
+function getCompleteExcuse(whoindex, whatindex, whenindex) {
+  return whoindex + " " + whatindex + " " + whenindex;
+}
+
+function getWhen() {
+  return getRandomNumber(when);
+}
+
+function getWhat() {
+  return getRandomNumber(what);
+}
+
+function getWho() {
+  return getRandomNumber(who);
+}
+
