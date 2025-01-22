8541643bdd5324cc2c8e735c1c8d6e461f38d410
Diego-1984 <diegomenoruceta@gmail.com>
2025-01-22 17:12:15 +0000

    Introducción de logo y corrección de errores gramaticales

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const who = ["The dog", "My granma", "His turtle", "My bird"];
-const what = ["eat", "pissed", "crushed", "broked"];
+const what = ["ate it", "pissed it ", "crushed it", "broken it"];
 const when = [
   "before the class!",
   "right in time!",
-  "when I finished!",
+  "when I finished it!",
   "during my lunch!",
   "while I was praying!"
 ];
